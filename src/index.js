d48f0e429486bff9a4973839ae091d9f023ad172
cy6erninja <cy6erninja@gmail.com>
2022-02-06 13:02:26 +0200

    The first implementation.

---
+++ src/index.js
@@ -0,0 +1,125 @@
+// Importing all external libraries that we will use.
+import Web3 from "web3";
+import dotenv from 'dotenv';
+import * as fs from 'fs/promises';
+
+// Loading .env variables.
+dotenv.config();
+
+const wethContractABI = [{ "inputs": [{ "internalType": "address", "name": "childChainManager", "type": "address" }], "stateMutability": "nonpayable", "type": "constructor" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "owner", "type": "address" }, { "indexed": true, "internalType": "address", "name": "spender", "type": "address" }, { "indexed": false, "internalType": "uint256", "name": "value", "type": "uint256" }], "name": "Approval", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": false, "internalType": "address", "name": "userAddress", "type": "address" }, { "indexed": false, "internalType": "address payable", "name": "relayerAddress", "type": "address" }, { "indexed": false, "internalType": "bytes", "name": "functionSignature", "type": "bytes" }], "name": "MetaTransactionExecuted", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "bytes32", "name": "role", "type": "bytes32" }, { "indexed": true, "internalType": "bytes32", "name": "previousAdminRole", "type": "bytes32" }, { "indexed": true, "internalType": "bytes32", "name": "newAdminRole", "type": "bytes32" }], "name": "RoleAdminChanged", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "bytes32", "name": "role", "type": "bytes32" }, { "indexed": true, "internalType": "address", "name": "account", "type": "address" }, { "indexed": true, "internalType": "address", "name": "sender", "type": "address" }], "name": "RoleGranted", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "bytes32", "name": "role", "type": "bytes32" }, { "indexed": true, "internalType": "address", "name": "account", "type": "address" }, { "indexed": true, "internalType": "address", "name": "sender", "type": "address" }], "name": "RoleRevoked", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "from", "type": "address" }, { "indexed": true, "internalType": "address", "name": "to", "type": "address" }, { "indexed": false, "internalType": "uint256", "name": "value", "type": "uint256" }], "name": "Transfer", "type": "event" }, { "inputs": [], "name": "CHILD_CHAIN_ID", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "CHILD_CHAIN_ID_BYTES", "outputs": [{ "internalType": "bytes", "name": "", "type": "bytes" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "DEFAULT_ADMIN_ROLE", "outputs": [{ "internalType": "bytes32", "name": "", "type": "bytes32" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "DEPOSITOR_ROLE", "outputs": [{ "internalType": "bytes32", "name": "", "type": "bytes32" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "ERC712_VERSION", "outputs": [{ "internalType": "string", "name": "", "type": "string" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "ROOT_CHAIN_ID", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "ROOT_CHAIN_ID_BYTES", "outputs": [{ "internalType": "bytes", "name": "", "type": "bytes" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "owner", "type": "address" }, { "internalType": "address", "name": "spender", "type": "address" }], "name": "allowance", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "spender", "type": "address" }, { "internalType": "uint256", "name": "amount", "type": "uint256" }], "name": "approve", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "account", "type": "address" }], "name": "balanceOf", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "decimals", "outputs": [{ "internalType": "uint8", "name": "", "type": "uint8" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "spender", "type": "address" }, { "internalType": "uint256", "name": "subtractedValue", "type": "uint256" }], "name": "decreaseAllowance", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "user", "type": "address" }, { "internalType": "bytes", "name": "depositData", "type": "bytes" }], "name": "deposit", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "userAddress", "type": "address" }, { "internalType": "bytes", "name": "functionSignature", "type": "bytes" }, { "internalType": "bytes32", "name": "sigR", "type": "bytes32" }, { "internalType": "bytes32", "name": "sigS", "type": "bytes32" }, { "internalType": "uint8", "name": "sigV", "type": "uint8" }], "name": "executeMetaTransaction", "outputs": [{ "internalType": "bytes", "name": "", "type": "bytes" }], "stateMutability": "payable", "type": "function" }, { "inputs": [], "name": "getChainId", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "pure", "type": "function" }, { "inputs": [], "name": "getDomainSeperator", "outputs": [{ "internalType": "bytes32", "name": "", "type": "bytes32" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "user", "type": "address" }], "name": "getNonce", "outputs": [{ "internalType": "uint256", "name": "nonce", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "bytes32", "name": "role", "type": "bytes32" }], "name": "getRoleAdmin", "outputs": [{ "internalType": "bytes32", "name": "", "type": "bytes32" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "bytes32", "name": "role", "type": "bytes32" }, { "internalType": "uint256", "name": "index", "type": "uint256" }], "name": "getRoleMember", "outputs": [{ "internalType": "address", "name": "", "type": "address" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "bytes32", "name": "role", "type": "bytes32" }], "name": "getRoleMemberCount", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "bytes32", "name": "role", "type": "bytes32" }, { "internalType": "address", "name": "account", "type": "address" }], "name": "grantRole", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "bytes32", "name": "role", "type": "bytes32" }, { "internalType": "address", "name": "account", "type": "address" }], "name": "hasRole", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "spender", "type": "address" }, { "internalType": "uint256", "name": "addedValue", "type": "uint256" }], "name": "increaseAllowance", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "name", "outputs": [{ "internalType": "string", "name": "", "type": "string" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "bytes32", "name": "role", "type": "bytes32" }, { "internalType": "address", "name": "account", "type": "address" }], "name": "renounceRole", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "bytes32", "name": "role", "type": "bytes32" }, { "internalType": "address", "name": "account", "type": "address" }], "name": "revokeRole", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "symbol", "outputs": [{ "internalType": "string", "name": "", "type": "string" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "totalSupply", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "recipient", "type": "address" }, { "internalType": "uint256", "name": "amount", "type": "uint256" }], "name": "transfer", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "sender", "type": "address" }, { "internalType": "address", "name": "recipient", "type": "address" }, { "internalType": "uint256", "name": "amount", "type": "uint256" }], "name": "transferFrom", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "uint256", "name": "amount", "type": "uint256" }], "name": "withdraw", "outputs": [], "stateMutability": "nonpayable", "type": "function" }];
+
+// WETH contract address on polygon.
+const wethContractAddress = process.env.ERC20_CONTRACT_ADDRESS;
+
+// Create web3 object using INFURA rpc.
+// INFURA_POLYGON_MAINNET=https://polygon-mainnet.infura.io/v3/{your-infura-id} is a variable defined in .env file.
+const web3 = new Web3(new Web3.providers.HttpProvider(process.env.INFURA_POLYGON_MAINNET));
+
+// Address we send our tokens to.
+//const toAddress = "0x...123"; 
+const toAddress = process.argv.slice(2).pop(0);
+
+if (!web3.utils.isAddress(toAddress)) {
+    console.log('The argument is not a valid address!');
+    process.exit();
+}
+
+// Reading private keys from file.
+const loadPrivateKeys = async () => {
+    // We add another .env config for filepath to our private key file since it may be different on different machines.
+    const privateKeysFilePath = process.env.PRIVATE_KEYS_FILE_PATH;
+    let privateKeys = [];
+
+    try {
+        privateKeys = await fs.readFile(privateKeysFilePath, "utf-8");
+
+        // We assume that every key is on it's own line, so we split file by new line symbol and remove last empty line using filter.
+        privateKeys = privateKeys.split('\n').filter(e => e);
+    } catch (e) {
+        // If we can't read private keys, exit.
+        console.log("Can't load private keys!", e);
+        process.exit();
+    }
+
+    return privateKeys;
+}
+
+// If we don't tell our contract, what address we are sending tokens from, it 
+// will say that we should not send from zero address(even though we 
+// sign our transaction with private key.)
+const createWethContract = (fromAddress) => {
+    return new web3.eth.Contract(
+        wethContractABI,
+        wethContractAddress,
+        { from: fromAddress }
+    )
+}
+
+const signTransaction = async (wethContractObject, fromPrivateKey, toAddress, balance) => {
+    const encodedTransferABI = wethContractObject.methods.transfer(toAddress, balance).encodeABI();
+    const estimatedGas = await wethContractObject.methods.transfer(toAddress, balance).estimateGas({
+        to: wethContractAddress,
+        data: encodedTransferABI,
+    });
+
+    const Tx = {
+        // We are sending request to WETH contract, asking it to transfer tokens.
+        to: wethContractAddress,
+        gas: estimatedGas,
+        // We send zero native token, which is Matic on Polygon network.
+        value: "0x0",
+        // We set only tip for the miner, baseFeePerGas(which is burned) will be set automatically.
+        // In order to send legacy transaction(before London fork), you can use gas and gasPrice.
+        maxPriorityFeePerGas: await web3.eth.getGasPrice(),
+        data: `${encodedTransferABI}`,
+    };
+
+    const signTransactionOutput = await web3.eth.accounts.signTransaction(
+        Tx,
+        fromPrivateKey
+    );
+
+    return signTransactionOutput.rawTransaction;
+};
+
+const sendTransaction = (rawTransaction) => {
+    return web3.eth.sendSignedTransaction(
+        rawTransaction
+    ).once('transactionHash', (function (hash) { console.log(`Tx hash: ${hash}`) }))
+        .on('confirmation', function (confNumber, receipt) { console.log(`Confirmation: ${confNumber}`); })
+        .on('error', async function (error) {
+            console.log('Something went wrong...', error);
+        });
+}
+
+// Here is our main flow in this function.
+const run = async () => {
+    // Loading private keys from a file.
+    const privateKeys = await loadPrivateKeys();
+
+    // Executing actions for each wallet we collect tokens from.
+    for (let i = 0; i < privateKeys.length; i++) {
+        let privateKey = privateKeys[i];
+        let walletAddress = web3.eth.accounts.privateKeyToAccount(privateKey).address;
+        let wethContractObject = createWethContract(walletAddress);
+        let balance = await wethContractObject.methods.balanceOf(walletAddress).call();
+
+        // Do not send tokens to the same address.
+        if (walletAddress === toAddress) {
+            continue;
+        }
+
+        // If balance is 0, then just skip this wallet.
+        if (balance <= 0) {
+            continue;
+        }
+
+        // Now we sign transaction and get raw transaction object, because we will use it later.
+        const rawTransaction = await signTransaction(wethContractObject, privateKey, toAddress, balance);
+        //And finally sending transaction to the mempool.
+        sendTransaction(rawTransaction);
+    }
+};
+
+// Executing script.
+run();
